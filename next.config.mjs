169bb3e2b1c3afa699f1545ba64df19ed12b3922
narman2211 <narman2211@gmail.com>
2026-03-12 01:24:07 +0000

    feat: Restore and enrich 14 blog posts, fix redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,52 @@
+
 /** @type {import('next').NextConfig} */
-// Triggering a server restart to apply image domain configuration.
 const nextConfig = {
     skipTrailingSlashRedirect: true,
     images: {
         remotePatterns: [
             {
-                protocol: 'https',
-                hostname: 'e7.pngegg.com',
-                port: '',
-                pathname: '/**',
+                protocol: 'https'',
+                hostname: 'e7.pngegg.com'',
+                port: '''',
+                pathname: '/**''',
             },
             {
-                protocol: 'https',
-                hostname: 'i.imgur.com',
-                port: '',
-                pathname: '/**',
+                protocol: 'https''',
+                hostname: 'i.imgur.com''',
+                port: '''',
+                pathname: '/**''',
             },
         ],
+    },
+    async redirects() {
+        return [
+          {
+            source: '/blog/why-cladding-is-best-for-riyadh'',
+            destination: '/blog/cladding-comparison-saudi-american-chinese''',
+            permanent: true,
+          },
+          {
+            source: '/services/signage-manufacturing''',
+            destination: '/services/advertising-signs''',
+            permanent: true,
+          },
+          {
+            source: '/services/advertising-boards''',
+            destination: '/services/advertising-signs''',
+            permanent: true,
+          },
+          {
+            source: '/services/maintenance-and-restoration''',
+            destination: '/services/maintenance-restoration''',
+            permanent: true,
+          },
+          {
+            source: '/contact''',
+            destination: '/#contact''',
+            permanent: true,
+          },
+        ]
     },
 };
 
 export default nextConfig;
-
-// Restarting server to load new .env.local variables
